9774268d367158fcbd6407d983b1c89aedf92928
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-10-01 00:26:12 -0700

    done: finished turtle race. might update later

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ screen = Screen()
 screen.colormode(255)
 
 
-
 # Methods
 
 def create_turtles(turtle_amt):
@@ -14,6 +13,7 @@ def create_turtles(turtle_amt):
         turtle_list_inner.append(Turtle())
         turtle_list_inner[i].shape("turtle")
         turtle_list_inner[i].penup()
+        turtle_list_inner[i].speed(3)
         turtle_list_inner[i].fillcolor((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
     return turtle_list_inner
 
@@ -28,6 +28,18 @@ def turtle_positions(turtle_list_second_inner):
     return turtle_list_second_inner
 
 
+def turtle_race(move_em):
+    finished = False
+    while not finished:
+        turtle_num = 1
+        for i in move_em:
+            i.forward(random.randint(0, 30))
+            if i.xcor() > 400:
+                finished = True
+                return turtle_num
+            turtle_num += 1
+
+
 # timeline
 
 print("Welcome to the turtle races!!")
@@ -37,6 +49,10 @@ who_will_win = int(input("Which turtle # do you think will win?: "))
 turtle_list = create_turtles(amount_turtles)
 screen.setup(width=400, height=((amount_turtles * 20) + 20), startx=0, starty=0)
 t_list_with_positions = turtle_positions(turtle_list)
-
+winner = turtle_race(t_list_with_positions)
+if winner == who_will_win:
+    print(f"you picked turtle #{winner}, the winner! get to the races!")
+else:
+    print(f"sorry chum, you picked {who_will_win}, but the winner was {winner}. good luck next time!")
 
 screen.exitonclick()
